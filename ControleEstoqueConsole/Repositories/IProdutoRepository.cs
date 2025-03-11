d8b1ad957fcd208a05b408cb998f3cb93eb82ca0
benicio <beniciobrandao@hotmail.com>
2025-03-11 13:31:12 -0300

    Criando o arquivo IProdutoRepository

--- ControleEstoqueConsole/Repositories/IProdutoRepository.cs
+++ ControleEstoqueConsole/Repositories/IProdutoRepository.cs
@@ -1,4 +1,11 @@
-﻿namespace ControleEstoqueConsole.Repositories;
-internal interface IProdutoRepository
+﻿using ControleEstoqueConsole.Domain.Entities;
+
+namespace ControleEstoqueConsole.Repositories;
+public interface IProdutoRepository
 {
+    Produto ObterPorId(int id);
+    List<Produto> ObterTodos();
+    void Adicionar(Produto produto);
+    void Atualizar(Produto produto);
+    void Remover(int id);
 }
