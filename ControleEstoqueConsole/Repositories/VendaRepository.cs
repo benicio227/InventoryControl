ef5f1298cb7e32566543f21f191d5d6a8981f06a
benicio <beniciobrandao@hotmail.com>
2025-03-12 10:11:39 -0300

    Implementação dos método ObterHistorico e RegistrarVenda

--- ControleEstoqueConsole/Repositories/VendaRepository.cs
+++ ControleEstoqueConsole/Repositories/VendaRepository.cs
@@ -1,4 +1,21 @@
-﻿namespace ControleEstoqueConsole.Repositories;
-internal class VendaRepository
+﻿using ControleEstoqueConsole.Domain.Entities;
+
+namespace ControleEstoqueConsole.Repositories;
+public class VendaRepository : IVendaRepository
 {
+    private readonly List<Venda> _venda = new();
+    private int _proximoId = 1;
+    public List<Venda> ObterHistorico()
+    {
+        return _venda;
+    }
+
+    public void RegistrarVenda(Venda venda)
+    {
+        venda.Id = _proximoId++;
+        _venda.Add(venda);
+
+        Console.WriteLine($"Venda registrada: {venda.Quantidade} unidade(s) de {venda.Nome} em {venda.Data:dd/MM/yyyy HH:mm}");
+    }
+
 }
