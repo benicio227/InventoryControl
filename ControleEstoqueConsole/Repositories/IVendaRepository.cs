bb36d08bffb2a22f579c7d432d980c72a40a8f9a
benicio <beniciobrandao@hotmail.com>
2025-03-12 09:40:15 -0300

    Criação dos metódos de contrato na interface IVendaRepository

--- ControleEstoqueConsole/Repositories/IVendaRepository.cs
+++ ControleEstoqueConsole/Repositories/IVendaRepository.cs
@@ -1,4 +1,8 @@
-﻿namespace ControleEstoqueConsole.Repositories;
-internal interface IVendaRepository
+﻿using ControleEstoqueConsole.Domain.Entities;
+
+namespace ControleEstoqueConsole.Repositories;
+public interface IVendaRepository
 {
+    void RegistrarVenda(Venda venda);
+    List<Venda> ObterHistorico();
 }
