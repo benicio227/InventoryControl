45def5db7333a1cd25281b45e1ce0acf025063a9
benicio <beniciobrandao@hotmail.com>
2025-03-12 09:28:04 -0300

    Teste das regras de negócio no arquivo Program.cs

--- ControleEstoqueConsole/Program.cs
+++ ControleEstoqueConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using ControleEstoqueConsole.Domain.Entities;
+using ControleEstoqueConsole.Repositories;
+using ControleEstoqueConsole.Services;
 
 namespace ControleEstoqueConsole
 {
@@ -6,7 +8,22 @@ namespace ControleEstoqueConsole
     {
         static void Main(string[] args)
         {
+            var produtoRepository = new ProdutoRepository();
+            var produtoService = new ProdutoService(produtoRepository);
 
+            produtoService.CadastrarProduto("Notebook", 3500, 10);
+            produtoService.CadastrarProduto("Mouse", 100, 50);
+
+            produtoService.ListarProdutos();
+
+            produtoService.VenderProduto(1, 2);
+            produtoService.AdicionarEstoque(2, 10);
+
+            produtoService.ListarProdutos();
+
+            produtoService.RemoverProduto(2);
+
+            produtoService.ListarProdutos();
         }
     }
 }
